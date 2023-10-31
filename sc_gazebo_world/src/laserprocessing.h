02b9f5fbbe07515b1d87b7be67637df2d0c29934
J-e-k <63453871+J-e-k@users.noreply.github.com>
2023-10-31 17:25:00 +1100

    Comments (#3)
    * pushing my changes
    
    
    
    ---------
    
    Co-authored-by: Jacinta Kuessner <jacinta.kuessner@student.uts.edu.au and jay>

--- sc_gazebo_world/src/laserprocessing.h
+++ sc_gazebo_world/src/laserprocessing.h
@@ -10,15 +10,15 @@
 /*!
  *  \brief     Laser Processing Class
  *  \details
- *  This class is used for processing laser scan data and finding different things such as the locations of cones,
- *  the location of a goal, determining if a goal is between two cones, and converting laser data into different data types
- *  for other classes to understand. This class relies on the Sample class to input the laser data collected from a laser scanner
- *  inside a simulator that detects and measures object and their proximity to the sensor. It takes 640 readings in a 180 arc at
- *  the front of the car and has limited range.
+ *  This class is designed to process laser data obtained from an LDS-01 sensor mounted 
+ *  on a Turtlebot3 Waffle robot. The sensor captures readings in 360-degree increments 
+ *  with a precision of 1 degree. The class provides counting valid readings, identifying 
+ *  cone segments, finding distances based on angles, calculating turns, and determining 
+ *  the magnitude of turns.
  *  @sa Sample
- *  \author    Ashton Powell
+ *  \author    Ashton Powell, Jacinta Kuessner
  *  \version   1.00
- *  \date      2023-05-30
+ *  \date      31/10/23
  */
 
 class LaserProcessing
